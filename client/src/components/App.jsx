a6d189d4791c274557aba3e40736f0e47828a858
Reed Bruskin <reedbruskin@google.com>
2021-04-17 15:33:34 -0700

    Reformat App to use Hooks instead of a stateful component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,64 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import MortalityMap from './MortalityMap.jsx';
 import colorScaleData from '../helpers/colorScaleData.js';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.props.dummyData,
-      renderMap: false
-    };
+let App = (props) => {
+  const [data, setData] = useState(props.dummyData);
+  const [renderMap, setRenderMap] = useState(false);
 
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
-
-  componentDidMount() {
-    axios.get('/api/heartFailures')
-      .then((res) => {
-        console.log(res);
-        this.setState({
-          data: res.data,
-        })
-      })
-      .catch((err) => {
-        console.log(`Axios GET error: ${err}`)
-      })
-  }
-
-  handleButtonClick() {
-    this.setState({
-      renderMap: true
-    });
-  }
-
-  renderMap() {
-    if (!this.state.renderMap) {
+  let render = () => {
+    if (!renderMap) {
       return null;
     }
 
     return (
-      <MortalityMap stateMortalityScores={colorScaleData(this.state.data)}
-                    handleButtonClick={this.handleButtonClick}
+      <MortalityMap stateMortalityScores={colorScaleData(data)}
+                    handleButtonClick={() => {setRenderMap(true)}}
       />
     );
   }
 
-  render() {
-    console.log(this.state);
-    return (
-      <div>
-        <div className="nav">
-          <button onClick={this.handleButtonClick}>Render Map!</button>
-        </div>
-        <div className="map">
-          {this.renderMap()}
-        </div>
+  useEffect(() => {
+    axios.get('/api/heartFailures')
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(`Axios GET error: ${err}`)
+      })
+  }, [])
+
+  return (
+    <div>
+      <div className="nav">
+        <button onClick={() => {setRenderMap(true)}}>Render Map!</button>
       </div>
-    );
-  }
+      <div className="map">
+        {render()}
+      </div>
+    </div>
+  );
 }
 
 export default App;
